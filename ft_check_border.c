b0787bdd30aa35e49cdf00bbc65df725bd92f923
Pitak Niyom <pniyom@z3t1c2.42bangkok.com>
2023-02-11 16:55:13 +0700

    solved check border

--- ft_check_border.c
+++ ft_check_border.c
@@ -6,7 +6,7 @@
 /*   By: pniyom <pniyom@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 15:27:08 by pniyom            #+#    #+#             */
-/*   Updated: 2023/02/11 16:42:36 by pniyom           ###   ########.fr       */
+/*   Updated: 2023/02/11 16:53:24 by pniyom           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	check_border(t_game *game, int h)
 	int	row_len;
 
 	i = 0;
-	if (h == 0 || game->height - 1)
+	if (h == 0 || h == game->height - 1)
 	{
 		while(game->map[h][i] != '\0' && game->map[h][i] != '\n')
 		{
@@ -35,14 +35,9 @@ void	check_border(t_game *game, int h)
 	}
 	else
 	{
-		ft_printf("enter second case\n");
-		ft_printf("%s\n", game->map[h]);
 		row_len = ft_strlen(game->map[h]) - 1;
-		printf("strlen returned value = %d\n", row_len);
 		if(game->map[h][0] != '1' || game->map[h][row_len - 1] != '1')
 			error_border(game);
-		else
-			ft_printf("border is OK\n");
 	}
 }
 
@@ -51,11 +46,8 @@ void	ft_check_border(t_game *game)
 	int	h;
 	
 	h = 0;
-	ft_printf("game->height = %d\n", game->height);
 	while(h < game->height)
 	{
-		ft_printf("h = %d\n", h);
-		ft_printf("%s\n", game->map[h]);
 		check_border(game, h);
 		h++;
 	}
