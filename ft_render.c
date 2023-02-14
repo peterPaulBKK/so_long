e032e580a3e0db4767cea41eecb0a50d7296f4a1
Pitak Niyom <peterPaul@MacBook-Pro-khxng-Sutanya.local>
2023-02-14 13:54:48 +0700

    after new batch meeting

--- ft_render.c
+++ ft_render.c
@@ -6,7 +6,7 @@
 /*   By: pniyom <pniyom@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 02:26:22 by pniyom            #+#    #+#             */
-/*   Updated: 2023/02/12 02:42:06 by pniyom           ###   ########.fr       */
+/*   Updated: 2023/02/12 14:39:02 by pniyom           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,11 @@ void	ft_render(t_game *game, t_map *definitive_map)
 	definitive_map->mlx = mlx_init();
 	definitive_map->win = mlx_new_window(definitive_map->mlx, (game->len - 1) * 64, \
 	(game->height - 1) * 64, "Game");
+	definitive_map->wall = mlx_xpm_file_to_image(definitive_map->mlx, "img/wall.xpm", \
+	&game->len, &game->height);
+	definitive_map->floor = mlx_xpm_file_to_image(definitive_map->mlx, \
+	"./img/floor64.xpm", &game->len, &game->height);
+	definitive_map->player = mlx_xpm_file_to_image(definitive_map->mlx, \
+	"./img/floor64.xpm", &game->len, &game->height);)
+	// mlx_hook(definitive_map->win, 17, 0L, 
 }
